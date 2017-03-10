44bd195c991c112d05b1cc6e7b537c0d78cd9c9b
Elia Martell <login+martell@elo7.com>
2017-03-10 11:04:25 -0300

    Cria teste para Rounds

---
+++ board-game/src/test/java/com/elo7/board_game/games/RoundsTest.java
@@ -0,0 +1,16 @@
+package com.elo7.board_game.games;
+
+import org.junit.Test;
+
+public class RoundsTest {
+
+	@Test
+	public void shouldReturnUsersScoreInARound() {
+
+	}
+
+	@Test
+	public void shouldReturnFirstUserRound() {
+
+	}
+}
